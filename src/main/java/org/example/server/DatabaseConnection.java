08640a4ca7d38ff4dbd8e65dd27e0fb2a57339aa
Candra Wikananta <candrawikananta@gmail.com>
2024-06-20 01:58:06 +0800

    Bug Fix pada database

--- src/main/java/org/example/server/DatabaseConnection.java
+++ src/main/java/org/example/server/DatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:sqlite:C:/Java Program/test API/subscription.db";
+    private static final String URL = "jdbc:sqlite:db_subscription.db";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
